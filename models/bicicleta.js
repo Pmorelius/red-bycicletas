592b29d6be51c927c9cc60d64837594721d96e97
Pmorelius <foxtrot@misena.edu.co>
2020-12-14 18:12:46 -0500

    Se realiza el metodo CRUD, queda funcional

--- models/bicicleta.js
+++ models/bicicleta.js
@@ -1,5 +1,5 @@
 //este proceso se realiza para avanzar sin haber creado una base de datos
-var Bicicleta = function (id, modelo, ubicacion) {
+var Bicicleta = function (id, color, modelo, ubicacion) {
 	this.id = id;
 	this.color = color;
 	this.modelo = modelo;
@@ -10,16 +10,40 @@ Bicicleta.prototype.toString = function (){
 	return 'id: ' + this.id + " | color: " + this.color;
 }
 
+/* este arreglo tambien sirve en la linea .9
+Bicicleta.prototype.toString =function(){
+    return `id: ${this.id}|color ${this.color}`;*/
+
 Bicicleta.allBicis = [];
 Bicicleta.add = function(aBici){
 	Bicicleta.allBicis.push(aBici);
 }
+
+Bicicleta.findById = function(aBiciId) {
+    var aBici = Bicicleta.allBicis.find( x=> x.id == aBiciId);
+    if (aBici ) 
+        return aBici;
+
+    else
+      	throw new Error(`No existe bicicleta con el id ${aBiciId}`)
+}
+
+Bicicleta.removeById = function(aBiciId) {
+    // var aBici = Bicicleta.findById(aBiciId)
+    for(var i = 0; i < Bicicleta.allBicis.length; i ++) {
+        if(Bicicleta.allBicis[i].id == aBiciId){
+            Bicicleta.allBicis.splice(i,1)
+            break;
+        }
+    }
+}
+
+
 
 var a = new Bicicleta(1, 'rojo', 'urbana', [3.472695,-76.4950865]);
 var b = new Bicicleta(2, 'blanca', 'urbana', [3.4728267,-76.4933002]);
 
 Bicicleta.add(a);
 Bicicleta.add(b);
-
 
 module.exports = Bicicleta;
