4d0018e7471b397962007c131a8b93daadf7fde4
Pmorelius <foxtrot@misena.edu.co>
2021-01-04 21:14:38 -0500

    verificacion usuarios

--- models/usuario.js
+++ models/usuario.js
@@ -1,9 +1,11 @@
 var mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 var Reserva  = require('./reserva');
 var Schema = mongoose.Schema;
 const bcrypt = require('bcrypt');
-
 const saltRounds = 10;
+
+var Schema = mongoose.Schema;
 
 const validateEmail = function(email) {
 	const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
@@ -21,6 +23,7 @@ var usuarioSchema = new Schema({
     	trin: true,
     	required: [true, 'El email es obligatorio'],
     	lowercase; true,
+    	unique: true,
     	validate: [validateEmail, 'Por favor, ingrese un email valido']
     	match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/]
     },
@@ -36,6 +39,8 @@ var usuarioSchema = new Schema({
     }
 });
 
+usuarioSchema.plugin(uniqueValidator, {message: 'El {PATH} ya existe con otro usuario.' });
+
 
 usuarioSchema.pre('save', function(next){ 
 	if (this.ismodified('password')){
@@ -44,10 +49,38 @@ usuarioSchema.pre('save', function(next){
 	next();
 });
 
+usuarioSchema.methods.validPassword = function(password){
+	return bcrypt.compareSync(password, this.password);
+}
+
 usuarioSchema.methods.reservar = function(biciId, desde, hasta, cb){
     var reserva = new Reserva({usuario: this._id, bicicleta: biciId, desde: desde, hasta: hasta});
     console.log('reserva');
     reserva.save(cb);
 }
+
+usuarioSchema.methods.enviar_email_bienvenidad = function(cb) {
+    const token = new Token({_userId: this.id, token: crypto.randonBytes(16).toString
+    const email_destination = this.email;
+    token.save(function(err){ 
+        if (err) { return console.log(err.message); }
+
+        const mailOptions = {
+            from: 'no-reply@bicicletas.com',
+            to: email_destination,
+            subject: 'Verificacion de cuenta',
+            text: 'Hola,\n\n' + 'Por favor, para verificar tu cuenta haga click en este link: \n' + 'http://localhost:5000' + '\/token/confirmation\/' + token.token + '\n'
+        );
+        mailer.sendMail(mailOptions, function (err){
+            if (err) { return console.log(err.message); }
+            console.log('Se ha enviado u email de bienvenida a:' + email_destination + '.');
+
+
+        });
+    });
+      
+}
+
+
 
 module.exports = mongoose.model('Usuario',usuarioSchema);
